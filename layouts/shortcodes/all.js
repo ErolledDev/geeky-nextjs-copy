e6d0553f69b3f6055e2aa6d0ea445030f10aa6f5
Cedrick A.k.a ErolledDev <107593932+ErolledDev@users.noreply.github.com>
2025-06-28 17:41:35 +0800

    Remove imports for deleted shortcode files

--- layouts/shortcodes/all.js
+++ layouts/shortcodes/all.js
@@ -1,13 +1,9 @@
-import Button from "./Button.jsx";
 import Code from "./Code.jsx";
-import Notice from "./Notice.jsx";
 import Video from "./Video.jsx";
 import Youtube from "./Youtube.jsx";
 
 const shortcodes = {
-  Button,
   Video,
-  Notice,
   Code,
   Youtube
 };
